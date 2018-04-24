3f05513123c763699a5e240cd57f804e3a63f1b9
Leon Kladnik <Leon Kladnik>
2018-04-24 12:38:47 +0200

    Basisversion eines Clients in Go.

---
+++ Client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func request(sitename string) bool {
+	fmt.Printf("Calling %s...\n", sitename)
+	resp, err := http.Get(sitename)
+
+	if err != nil {
+		fmt.Printf("Failed to establish a connection.\n")
+		return false
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Could not read response body.\n")
+		return false
+	}
+
+	fmt.Printf("Connection successful!\nResponse: %s\n\n", body)
+	return true
+}
+
+func main() {
+	services := [6]string{
+		"https://ppl-customer.herokuapp.com/",
+		"https://ppl-pricecalculator.herokuapp.com/",
+		"https://ppl-billingandmailing.herokuapp.com/",
+		"https://ppl-leasing.herokuapp.com/",
+		"https://ppl-stock.herokuapp.com/",
+		"https://ppl-reservation.herokuapp.com/",
+	}
+	connected := 0
+	for _, service := range services {
+		if request(service) {
+			connected++
+		}
+	}
+	fmt.Print("Done.\n")
+	fmt.Printf("Successful connections: %d out of 6", connected)
+}
